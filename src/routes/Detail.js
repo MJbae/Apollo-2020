bc484e6e90ea85a2e813e36bdda6f4f97cc9db0c
MJbae <bmj5208@gmail.com>
2020-11-17 15:34:22 +0900

    style: Home, Detail Page 스타일 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,15 +1,52 @@
 import React from "react";
 import { useQuery, gql } from "@apollo/client";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     getOneMovie(id: $id) {
-      id
       title
       medium_cover_image
+      language
+      rating
+      summary
     }
   }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
 `;
 
 function Detail() {
@@ -17,7 +54,16 @@ function Detail() {
   const { loading, data } = useQuery(GET_MOVIE, {
     variables: { id: parseInt(id) },
   });
-  return <div>Detail</div>;
+  return (
+    <Container>
+      <Column>
+        <Title>Name</Title>
+        <Subtitle>English · 4.5</Subtitle>
+        <Description>lorem ipsum lalalla </Description>
+      </Column>
+      <Poster></Poster>
+    </Container>
+  );
 }
 
 export default Detail;
