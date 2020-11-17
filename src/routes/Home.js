eb79eddfce022d514dde913784e4ec4fefa7b430
MJbae <bmj5208@gmail.com>
2020-11-17 09:27:27 +0900

    feat: getAllMovies Query 추가 in Client Side

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,25 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
-export default () => "Home";
+const GET_MOVIES = gql`
+  {
+    getAllMovies {
+      id
+      title
+      medium_cover_image
+    }
+  }
+`;
+
+const Home = () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  console.log(loading, data);
+  if (loading) {
+    return <h2>loading...</h2>;
+  }
+  if (data && data.getAllMovies) {
+    return data.getAllMovies.map((m) => <h1>{m.id}</h1>);
+  }
+};
+
+export default Home;
